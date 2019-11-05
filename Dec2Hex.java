54adc1afe345d73f80f7b42c39f760abb850c093
Fleep-6 <56159619+Fleep-6@users.noreply.github.com>
2019-11-05 12:17:02 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -32,13 +32,12 @@ class Dec2Hex
 	    hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
-	    if(hexadecimal.isEmpty())
+	if(!hexadecimal.isEmpty())
         {
-            System.out.println("A value must be entered");
-            return;
+            System.out.println("Hexadecimal representation is : " + hexadecimal);            
         }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
-
-    }
+        else
+        {
+        System.out.println("A value must be entered");
+        }
 }
